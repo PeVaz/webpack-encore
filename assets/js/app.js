be039e4c3b9b4ecfcacad3e439dedc250730c00c
Pedro Vázquez <pvazquez0918@gmail.com>
2020-08-24 16:46:49 +0200

    Bootstrap and jQuery Plugins

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,9 +9,19 @@
 import '../css/app.css';
 
 import $ from 'jquery';
+import 'bootstrap'; // adds functions to jQuery
+
 // uncomment if you have legacy code that needs global variables
 //global.$ = $;
 
 import getNiceMessage from './get_nice_message';
 
 console.log(getNiceMessage(5));
+
+$('.dropdown-toggle').dropdown();
+$('.custom-file-input').on('change', function(event) {
+    var inputFile = event.currentTarget;
+    $(inputFile).parent()
+        .find('.custom-file-label')
+        .html(inputFile.files[0].name);
+});
